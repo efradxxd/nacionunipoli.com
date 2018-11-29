64cf60a110c721dda51c7407c1785d1f3df5906c
efradxxd <33165770+efradxxd@users.noreply.github.com>
2018-11-28 22:06:14 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,5 @@
 <?php 
-$conexion=mysqli_connect('login.cysbrtfteyny.us-east-1.rds.amazonaws.com','root','root','login') or die('No se puede conectar a la base de datos'.mysql_error($conexion));
+
+$conexion=mysqli_connect('login.cysbrtfteyny.us-east-1.rds.amazonaws.com:3306','efradxxd','Supergato98%','login') or die('No se puede conectar a la base de datos'.mysql_error($conexion));
+
  ?>
