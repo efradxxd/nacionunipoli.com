7ff42ca7f981fdcbc01cda1c13ec1d6aa6593de0
efradxxd <33165770+efradxxd@users.noreply.github.com>
2018-11-28 21:59:55 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php 
-$conexion=mysqli_connect('localhost','root','root','login') or die('No se puede conectar a la base de datos'.mysql_error($conexion));
+$conexion=mysqli_connect('login.cysbrtfteyny.us-east-1.rds.amazonaws.com','root','root','login') or die('No se puede conectar a la base de datos'.mysql_error($conexion));
  ?>
